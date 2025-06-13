e368bfef326a5d7e30ed40c88e1359c6ed245ebf
giap <giapfc123@gmail.com>
2025-06-13 23:01:10 +0700

    Update vite.config.js to include server settings and adjust build input paths; update index.html to use the latest Font Awesome version and improve form input styles; refine style.css for better layout consistency.

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,22 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [tailwindcss()],
-  root: 'src',
-  base: './',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html'),
-        'pages/home/password-verification': resolve(__dirname, 'src/pages/home/password-verification.html'),
-        'pages/home/two-factor-auth': resolve(__dirname, 'src/pages/home/two-factor-auth.html'),
-      },
+    plugins: [tailwindcss()],
+    root: 'src',
+    base: './',
+    server: {
+        allowedHosts: true
     },
-  },
-})
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                'pages/home/password-verification': resolve(__dirname, 'src/pages/home/password-verification.html'),
+                'pages/home/two-factor-auth': resolve(__dirname, 'src/pages/home/two-factor-auth.html')
+            }
+        }
+    }
+});
